f8fb07ef24bc6154e0d81a2bc2510c0174a828e0
webalar <68447259+webalar@users.noreply.github.com>
2021-05-21 11:02:16 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "normalize.css";
+import "./css/main.ltr.css";
+import "./css/themes.css";
+import "./css/orange.css";
+import "./css/custom.css";
+
+import SiteState from './js/SiteState';
+import Site from './js/Site';
+
+const siteState = new SiteState();
+new Site(siteState);
